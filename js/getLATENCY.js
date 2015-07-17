e291c1d44d28906fad521d6ce35430fc1be7b0a2
catscarlet <i@catscarlet.com>
2015-07-17 11:13:50 +0800

    Update js files . Beautify it by atom-beautify and linter-jscs .

--- js/getLATENCY.js
+++ js/getLATENCY.js
@@ -1,143 +1,141 @@
 function MyX(whichjson) {
 
-	var MyXA = new Array();
-	$.ajaxSettings.async = false;
-	$.getJSON("./shell/pingresult/" + whichjson, function(data) {
-		$.each(data, function(infoIndex, info) {
-			MyXA[infoIndex] = info["TIME"];
+    var MyXA = new Array();
+    $.ajaxSettings.async = false;
+    $.getJSON('./shell/pingresult/' + whichjson, function (data) {
+        $.each(data, function (infoIndex, info) {
+            MyXA[infoIndex] = info.TIME;
 
-		})
-
-	})
-
-	return MyXA;
+        });
+    });
+    return MyXA;
 }
 
 function MyYLATENCY(whichjson) {
-	var MyA = new Array();
-	var str = new Number;
-	$.ajaxSettings.async = false;
-	$.getJSON("./shell/pingresult/" + whichjson, function(data) {
-		$.each(data, function(infoIndex, info) {
-			str = Math.round(info["avg"]);
+    var MyA = new Array();
+    var str = new Number;
+    $.ajaxSettings.async = false;
+    $.getJSON('./shell/pingresult/' + whichjson, function (data) {
+        $.each(data, function (infoIndex, info) {
+            str = Math.round(info.avg);
 
-			MyA[infoIndex] = str;
+            MyA[infoIndex] = str;
 
-			//alert(MyA[infoIndex]);
-		})
+            // alert(MyA[infoIndex]);
+        });
 
-	})
+    });
 
-	return MyA;
+    return MyA;
+
 }
 
+$(function () {
+    var json2 = new Array();
 
-$(function() {
-	var json2 = new Array();
+    $('#LATENCY_container').highcharts({
 
-	//alert(MyY("server1.catscarlet.com.json"))
+        chart: {
+            type: 'spline',
+            renderTo: 'LATENCY_container'
+        },
 
-	$('#LATENCY_container').highcharts({
+        title: {
+            text: '服务器网络延迟',
+            x: -20 // center
+        },
+        subtitle: {
+            text: 'Source: pi.catscarlet.com',
+            x: -20
+        },
+        xAxis: {
+            categories: MyX('server2.catscarlet.com.json')
 
-		chart: {
-			type: 'spline',
-			renderTo: "LATENCY_container"
-		},
+        },
+        yAxis: {
+            title: {
+                text: '延迟(ms)'
+            },
 
-		title: {
-			text: '服务器网络延迟',
-			x: -20 //center
-		},
-		subtitle: {
-			text: 'Source: pi.catscarlet.com',
-			x: -20
-		},
-		xAxis: {
-			categories: MyX("server1.catscarlet.com.json")
-			//categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-		},
-		yAxis: {
-			title: {
-				text: '延迟(ms)'
-			},
+            value: 0,
+            width: 1,
+            color: '#808080'
 
-			value: 0,
-			width: 1,
-			color: '#808080'
+        },
+        tooltip: {
+            valueSuffix: 'ms'
+        },
+        legend: {
+            enabled: true,
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        plotOptions: {
+            spline: {
+                lineWidth: 2,
+                states: {
+                    hover: {
+                        lineWidth: 5
+                    }
+                },
+                marker: {
+                    enabled: false
+                },
+                // pointInterval: 3600000, // one hour
+                // pointStart: Date.UTC(2009, 9, 6, 0, 0, 0)
+            }
+        },
+        series: [{
+                name: 'server2.catscarlet.com',
+                data: MyYLATENCY('server2.catscarlet.com.json')
+            },
+            /*
+                        		{
+                        			name: 'BudgetVM - Chicago',
+                        			data: MyYLATENCY('199.231.208.6.json')
+                        		},
 
-		},
-		tooltip: {
-			valueSuffix: 'ms'
-		},
-		legend: {
-			enabled: true,
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'middle',
-			borderWidth: 0
-		},
-		plotOptions: {
-			spline: {
-				lineWidth: 2,
-				states: {
-					hover: {
-						lineWidth: 5
-					}
-				},
-				marker: {
-					enabled: false
-				},
-				//pointInterval: 3600000, // one hour
-				//pointStart: Date.UTC(2009, 9, 6, 0, 0, 0)
-			}
-		},
-		series: [{
-				name: 'server1.catscarlet.com',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+                        		{
+                        			name: 'BudgetVM - Dallas',
+                        			data: MyYLATENCY('192.80.186.135.json')
+                        		},
+                        */
+            {
+                name: 'BudgetVM - LosAngeles',
+                data: MyYLATENCY('192.157.214.6.json')
+            },
+            /*
+                        		{
+                        			name: 'BudgetVM - Miami',
+                        			data: MyYLATENCY('172.246.125.7.json')
+                        		},
 
-				data: MyYLATENCY("server1.catscarlet.com.json")
-			}, {
-				name: 'BudgetVM - Chicago',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLATENCY("199.231.208.6.json")
-			}, {
-				name: 'BudgetVM - Dallas',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLATENCY("192.80.186.135.json")
-			}, {
-				name: 'BudgetVM - LosAngeles',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLATENCY("192.157.214.6.json")
-			}, {
-				name: 'BudgetVM - Miami',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLATENCY("172.246.125.7.json")
-			}, {
-				name: 'Bandwagon Host - Phoenix',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLATENCY("198.35.46.10.json")
-			}, {
-				name: 'Bandwagon Host - LosAngeles',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLATENCY("104.194.76.20.json")
-			}, {
-				name: 'Bandwagon Host - Florida',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLATENCY("23.252.104.22.json")
-			}, {
-				name: 'Bandwagon Host - Holland',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLATENCY("192.243.124.74.json")
-			}
+                        		{
+                        			name: 'Bandwagon Host - Phoenix',
+                        			data: MyYLATENCY('198.35.46.10.json')
+                        		},
+                        */
+            {
+                name: 'Bandwagon Host - LosAngeles',
+                data: MyYLATENCY('104.194.76.20.json')
+            },
+            /*
+                        		{
+                        			name: 'Bandwagon Host - Florida',
+                        			data: MyYLATENCY('23.252.104.22.json')
+                        		},
 
-		]
-	});
+                        		{
+                        			name: 'Bandwagon Host - Holland',
+                        			data: MyYLATENCY('192.243.124.74.json')
+                        		},
+                        */
+            {
+                name: 'Bandwagon Host - Arizona',
+                data: MyYLATENCY('198.35.46.1.json')
+            }
+        ]
+    });
 });
--- js/getLOSS.js
+++ js/getLOSS.js
@@ -1,141 +1,140 @@
 function MyX(whichjson) {
 
-	var MyXA = new Array();
-	$.ajaxSettings.async = false;
-	$.getJSON("./shell/pingresult/" + whichjson, function(data) {
-		$.each(data, function(infoIndex, info) {
-			MyXA[infoIndex] = info["TIME"];
+    var MyXA = new Array();
+    $.ajaxSettings.async = false;
+    $.getJSON('./shell/pingresult/' + whichjson, function (data) {
+        $.each(data, function (infoIndex, info) {
+            MyXA[infoIndex] = info.TIME;
 
-		})
+        });
 
-	})
+    });
 
-	return MyXA;
+    return MyXA;
 }
 
 function MyYLOSS(whichjson) {
-	var MyA = new Array();
-	var str = new Number;
-	$.ajaxSettings.async = false;
-	$.getJSON("./shell/pingresult/" + whichjson, function(data) {
-		$.each(data, function(infoIndex, info) {
-			str = info["LOSS"].replace(/%/, "");
+    var MyA = new Array();
+    var str = new Number;
+    $.ajaxSettings.async = false;
+    $.getJSON('./shell/pingresult/' + whichjson, function (data) {
+        $.each(data, function (infoIndex, info) {
+            str = info.LOSS.replace(/%/, '');
 
-			MyA[infoIndex] = 100 - Number(str);
+            MyA[infoIndex] = 100 - Number(str);
 
-		})
+        });
 
-	})
+    });
 
-	return MyA;
+    return MyA;
 }
 
+$(function () {
+    var json2 = new Array();
 
-$(function() {
-	var json2 = new Array();
+    $('#LOSS_container').highcharts({
 
+        chart: {
+            type: 'spline',
+            renderTo: 'LOSS_container'
+        },
 
-	$('#LOSS_container').highcharts({
+        title: {
+            text: '服务器网络连通率',
+            x: -20 // center
+        },
+        subtitle: {
+            text: 'Source: pi.catscarlet.com',
+            x: -20
+        },
+        xAxis: {
+            categories: MyX('server2.catscarlet.com.json')
+        },
+        yAxis: {
+            title: {
+                text: '连通率 (%)'
+            },
 
-		chart: {
-			type: 'spline',
-			renderTo: "LOSS_container"
-		},
+            value: 0,
+            width: 1,
+            color: '#808080'
 
-		title: {
-			text: '服务器网络连通率',
-			x: -20 //center
-		},
-		subtitle: {
-			text: 'Source: pi.catscarlet.com',
-			x: -20
-		},
-		xAxis: {
-			categories: MyX("server1.catscarlet.com.json")
-				//            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-		},
-		yAxis: {
-			title: {
-				text: '连通率 (%)'
-			},
+        },
+        tooltip: {
+            valueSuffix: '%'
+        },
+        legend: {
+            enabled: true,
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        plotOptions: {
+            spline: {
+                lineWidth: 2,
+                states: {
+                    hover: {
+                        lineWidth: 5
+                    }
+                },
+                marker: {
+                    enabled: false
+                },
+                // pointInterval: 3600000, // one hour
+                // pointStart: Date.UTC(2009, 9, 6, 0, 0, 0)
+            }
+        },
+        series: [{
+                name: 'server2.catscarlet.com',
+                data: MyYLOSS('server2.catscarlet.com.json')
+            },
+            /*
+            		{
+            			name: 'BudgetVM - Chicago',
+            			data: MyYLOSS('199.231.208.6.json')
+            		},
 
-			value: 0,
-			width: 1,
-			color: '#808080'
+            		{
+            			name: 'BudgetVM - Dallas',
+            			data: MyYLOSS('192.80.186.135.json')
+            		},
+            */
+            {
+                name: 'BudgetVM - LosAngeles',
+                data: MyYLOSS('192.157.214.6.json')
+            },
+            /*
+            		{
+            			name: 'BudgetVM - Miami',
+            			data: MyYLOSS('172.246.125.7.json')
+            		},
 
-		},
-		tooltip: {
-			valueSuffix: '%'
-		},
-		legend: {
-			enabled: true,
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'middle',
-			borderWidth: 0
-		},
-		plotOptions: {
-			spline: {
-				lineWidth: 2,
-				states: {
-					hover: {
-						lineWidth: 5
-					}
-				},
-				marker: {
-					enabled: false
-				},
-				//pointInterval: 3600000, // one hour
-				//pointStart: Date.UTC(2009, 9, 6, 0, 0, 0)
-			}
-		},
-		series: [{
-				name: 'server1.catscarlet.com',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLOSS("server1.catscarlet.com.json")
-			}, {
-				name: 'BudgetVM - Chicago',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLOSS("199.231.208.6.json")
-			}, {
-				name: 'BudgetVM - Dallas',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLOSS("192.80.186.135.json")
-			}, {
-				name: 'BudgetVM - LosAngeles',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLOSS("192.157.214.6.json")
-			}, {
-				name: 'BudgetVM - Miami',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLOSS("172.246.125.7.json")
-			}, {
-				name: 'Bandwagon Host - Phoenix',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLOSS("198.35.46.10.json")
-			}, {
-				name: 'Bandwagon Host - LosAngeles',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLOSS("104.194.76.20.json")
-			}, {
-				name: 'Bandwagon Host - Florida',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLOSS("23.252.104.22.json")
-			}, {
-				name: 'Bandwagon Host - Holland',
-				//data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-				//data: MyyAxis
-				data: MyYLOSS("192.243.124.74.json")
-			}
+            		{
+            			name: 'Bandwagon Host - Phoenix',
+            			data: MyYLOSS('198.35.46.10.json')
+            		},
+            */
+            {
+                name: 'Bandwagon Host - LosAngeles',
+                data: MyYLOSS('104.194.76.20.json')
+            },
+            /*
+            		{
+            			name: 'Bandwagon Host - Florida',
+            			data: MyYLOSS('23.252.104.22.json')
+            		},
 
-		]
-	});
+            		{
+            			name: 'Bandwagon Host - Holland',
+            			data: MyYLOSS('192.243.124.74.json')
+            		},
+            */
+            {
+                name: 'Bandwagon Host - Arizona',
+                data: MyYLOSS('198.35.46.1.json')
+            }
+        ]
+    });
 });
