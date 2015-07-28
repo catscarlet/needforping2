cce47c8b19b8430e4c06273d2b0458eb9ed02a25
catscarlet <i@catscarlet.com>
2015-07-28 20:08:51 +0800

    增加数据库清理函数function pingresult_clean($q)，微调数据库调用语句

--- query.php
+++ query.php
@@ -6,7 +6,7 @@ define('DB_USER', 'needforping2');
 /** MySQL database password */
 define('DB_PASSWORD', 'needforping2');
 /** MySQL database table */
-$DB_TABLE="pingresult";
+define('DB_TABLE', 'pingresult');
 
 $q=$_GET["q"];
 $con = mysql_connect('localhost', constant('DB_USER'), constant('DB_PASSWORD'));
@@ -14,13 +14,15 @@ if (!$con)
  {
  die('Could not connect: ' . mysql_error());
  }
+mysql_select_db(constant('DB_NAME'), $con);
 
  /** 查询范围 */
 $query_range=720;
 
-mysql_select_db(constant('DB_NAME'), $con);
-$sql="select * from ".$DB_TABLE." where server_name = '".$q."' order by id DESC LIMIT $query_range";
+ /** 数据库清理函数 */
+//pingresult_clean($q);
 
+$sql="select * from ".constant('DB_TABLE')." where server_name = '".$q."' order by DATETIME DESC LIMIT $query_range";
 $result = mysql_query($sql);
 $i=min($query_range,mysql_num_rows($result));
 while($row = mysql_fetch_array($result) and $i >= 0)
@@ -42,3 +44,30 @@ $query_data=array_merge($query_data,$query_DATA);
 echo json_encode($query_data);
 
 mysql_close($con);
+
+
+function pingresult_clean($q)
+{
+    $sql="select COUNT(*) from ".constant('DB_TABLE')." where state = 'normal' and server_name =  '".$q."'";
+    $result = mysql_query($sql);
+    $row = mysql_fetch_array($result);
+
+     /** 保留记录条数，以30条为一小时的话，1440为2天 */
+    $retain_count=1440;
+
+    if ( $row['COUNT(*)'] >= $retain_count ){
+        $sql="select * from ".constant('DB_TABLE')." where server_name = '".$q."' order by DATETIME DESC LIMIT ".$retain_count ;
+        $result = mysql_query($sql);
+        while($row = mysql_fetch_array($result))
+         {
+             $lastrow_DATETIME=$row['DATETIME'];
+         }
+         $sql="update ".constant('DB_TABLE')." set state = 'delete' where server_name = '".$q."' and DATETIME <= '".$lastrow_DATETIME."'";
+         mysql_query($sql);
+
+         //$sql="DELETE FROM ".constant('DB_TABLE')."  where server_name = '".$q."' and state = 'delete'";
+        $sql="DELETE FROM ".constant('DB_TABLE')."  where state = 'delete'";
+
+         mysql_query($sql);
+    }
+}
